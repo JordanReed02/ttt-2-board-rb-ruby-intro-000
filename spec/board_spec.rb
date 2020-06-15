357595a47ef509b2101de8aa295897a92a32a94d
Jordan Reed <jordanalireed@icloud.com>
2020-06-15 22:49:03 +0000

    Automatically backed up by Learn

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -20,9 +20,8 @@ describe "lib/board.rb" do
   end
 
  it '`board` is an array with 9 strings with an empty space value, " "'
-
 do
 board = get_variable_from_file("./lib/board.rb", "board")
 
     expect(board).to match_array([" "," "," "," "," "," "," "," "," "])
-end 
+end end

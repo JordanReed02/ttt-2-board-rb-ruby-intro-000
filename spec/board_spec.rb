39ea123acb79ebdfb94aa77659644f7286fd9782
Jordan Reed <jordanalireed@icloud.com>
2020-06-15 22:36:42 +0000

    Automatically backed up by Learn

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,7 @@ describe "lib/board.rb" do
 'defines a local variable `board`' end
     board = get_variable_from_file("./lib/board.rb", "board")
 
-    'expect(board).to_not be_nil'end
+    'expect(board).to_not be_nil'
 
 
   it '`board` is set to an array' do
